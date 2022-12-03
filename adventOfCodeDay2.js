cc430c8950c6a49e1458bc8dfb3a8d2bbf37af19
unknown <benguiratoussama1998@gmail.com>
2022-12-03 14:27:30 +0100

    second day commit

---
+++ adventOfCodeDay2.js
@@ -0,0 +1,22 @@
+// advent of code day #2
+
+function greatestSum(string) {
+    let caloriesList = [];
+    let persons = string.split("\n\n");
+    for(let i = 0; i < persons.length - 1; i++) {
+        let meal = persons[i].split("\n");
+        let sum = 0;
+        for(let j = 0; j < meal.length; j++) {
+            sum += parseInt(meal[j]);
+        }
+        caloriesList.push(sum);
+    }
+    for(let k = 1; k < caloriesList.length; k++) {
+        for(let l = k-1; l > -1; l--) {
+            if(caloriesList[l+1] < caloriesList[l]) {
+                [caloriesList[l+1], caloriesList[l]] = [caloriesList[l], caloriesList[l+1]];
+            }
+        }
+    }
+    return caloriesList[caloriesList.length - 1] + caloriesList[caloriesList.length - 2] + caloriesList[caloriesList.length - 3];
+}
